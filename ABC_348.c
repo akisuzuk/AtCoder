e3b85d40b1f7c092f27c21d4daee403408c70cee
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2024-04-14 01:20:27 +0900

    0413 B

--- ABC_348.c
+++ ABC_348.c
@@ -25,11 +25,30 @@ int	main(void)
 
 //---B---
 
+typedef struct s_Coordinate
+{
+	int	x;
+	int	y;
+}	t_Coordinate;
+
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
+	int				max_dist;
+	int				n;
+	t_Coordinate	*coordinates;
+*
+	scanf("%d", n);
 
+	coordinates = (t_Coordinate *)malloc(n * sizeof(t_Coordinate));
+	if (coordinates == NULL)
+		return (1);
+
+	while()
+	{
+		77777777
+	}
 
 	return (0);
 }
