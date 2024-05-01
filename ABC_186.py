7c5a22f0133c3fab87a02cd6bdb66053355a5861
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2024-05-01 19:48:43 +0900

    pyton start

---
+++ ABC_186.py
@@ -0,0 +1,52 @@
+#==========================================================
+# [contest name]	ABC186
+# [question]		A
+# [title]			A-Brick
+# [language]		Python
+# [result]			AC/TLE/WA
+# [comment]			xxxx
+# [learned]			xxxx
+#==========================================================
+
+"""
+def divide (n, w):
+	#小数点以下切り捨て
+	return n //w
+
+def main():
+	n, w = map(int, input().split())
+	print(divide(n, w))
+
+if __name__ == "__main__":
+	main()
+"""
+
+#==========================================================
+# [contest name]	ABC186
+# [question]		B
+# [title]			Blocks on Grid
+# [language]		Python
+# [result]			AC/TLE/WA
+# [comment]			xxxx
+# [learned]			xxxx
+#==========================================================
+
+#def main():
+#	H, W = map(int, input().split())
+#	A = []
+#	A_min = 101
+#	for i in range(H):
+#		row = list(map(int, input().split()))
+#		if min(row) < A_min:
+#			A_min = min(row)
+#		A.append(row)
+#
+#	ans = 0
+#	for r in A:
+#		for e in r:
+#			if e > A_min:
+#				ans += e - A_min
+#	print(ans)
+#
+#if __name__ == "__main__":
+#	main()
---
+++ ABC_348.py
@@ -0,0 +1,10 @@
+#==========================================================
+# [contest name]	ABC348
+# [question]		D
+# [title]			Medicines on Grid
+# [language]		Python
+# [result]			AC/TLE/WA
+# [comment]			xxxx
+# [learned]			xxxx
+#==========================================================
+
