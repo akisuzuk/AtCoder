2e7c47992e3be42f4c4d4c3eac37147513af41b5
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2024-06-02 00:31:13 +0900

    ABC348 D

--- ABC_348.py
+++ ABC_348.py
@@ -4,7 +4,27 @@
 # [title]			Medicines on Grid
 # [language]		Python
 # [result]			AC/TLE/WA
-# [comment]			xxxx
+# [comment]			SHAKYO
 # [learned]			xxxx
 #==========================================================
 
+from heapq import heappop, heappush
+
+def main():
+	H, W = list(map(int, input().split()))
+	# list comprehensions内包表記
+	# 空白で区切られていないことに注意(split使わない)
+	A = [list(input()) for _ in range(H)]
+
+	for i in range(H):
+		for j in range(W):
+			#pythonってダブルクォートとシングルクォートどっちでもいいのか
+			if A[i][j] = 'S':
+				sy, sx = i, j
+			if A[i][j] = 'T':
+				gy, gx = i, j
+
+	N = int(input())
+	#---In Progress---
+if __name__ == "__main__":
+	main()
